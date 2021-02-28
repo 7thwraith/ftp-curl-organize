6367b20f42c381e2406b39f9d38748e5de71abe6
JT <7thwraith@gmail.com>
2021-02-28 10:04:18 -0500

    variable name changes

--- organize.sh
+++ organize.sh
@@ -3,7 +3,7 @@
 echo "Logging into FTP with curl"
 
 ## Log into FTP with curl, then find all files with the .mkv extension, and then dump the rsilt into text file called file-list.txt
-curl -l  ftp://${{ secrets.FTP_SERVER }}/${{ secrets.FTP_SUBDIR }}/ --user  ${{ secrets.FTP_USER }}:${{ secrets.FTP_PASSWORD }} | grep -oP '.*\.mkv' > file-list.txt
+curl -l  ftp://$FTP_SERVER/$FTP_SUBDIR/ --user  $FTP_USER:$FTP_PASSWORD | grep -oP '.*\.mkv' > file-list.txt
 
 
 while read filename
@@ -15,11 +15,11 @@ echo "$title" >> series.txt
 
 ## Create folders named after series
 echo "attempting to create folder named $title"
-curl -l  ftp:// ${{ secrets.FTP_SERVER }} --user  ${{ secrets.FTP_USER }}:${{ secrets.FTP_PASSWORD }} -Q "MKD ${{ secrets.FTP_SUBDIR}}/$title" --ftp-create-dirs
+curl -l  ftp:// $FTP_SERVER --user  $FTP_USER:$FTP_PASSWORD -Q "MKD $FTP_SUBDIR/$title" --ftp-create-dirs
 
 ## Move files into their respective series folders
 echo "moving files from Series folders to $title"
-curl -l  ftp:// ${{ secrets.FTP_SERVER }} --user  ${{ secrets.FTP_USER }}:${{ secrets.FTP_PASSWORD }} -Q "-RNFR ${{ secrets.FTP_SUBDIR}}/$filename" -Q "-RNTO ${{ secrets.FTP_SUBDIR}}/$title/$filename" --ftp-create-dirs
+curl -l  ftp:// $FTP_SERVER --user  $FTP_USER:$FTP_PASSWORD -Q "-RNFR $FTP_SUBDIR/$filename" -Q "-RNTO $FTP_SUBDIR/$title/$filename" --ftp-create-dirs
 
 
 done < file-list.txt
