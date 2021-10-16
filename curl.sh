5541ad233161479f2de1a31d0b6da04d8a74c154
JT <7thwraith@gmail.com>
2021-10-16 16:23:50 -0400

    added a job for curl refresh

---
+++ curl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Courtesy of https://gist.github.com/cdown/1163649
+urlencode() {
+    # urlencode <string>
+
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:$i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+
+    LC_COLLATE=$old_lc_collate
+}
+
+
+datastr="$(urlencode $USERFIELD)=$(urlencode $FTP_USER)&$(urlencode $PASSFIELD)=$(urlencode $FTP_PASSWORD)"
+
+curl --cookie-jar cookies.txt --data $datastr $DASHBOARD_URL
+curl --cookie cookies.txt $REFRESH_URL
